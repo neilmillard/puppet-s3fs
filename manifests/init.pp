b932d481bb0c886f513b19e22ef177a0b99317fe
neilmillard <neil.millard@gmail.com>
2015-10-22 22:31:42 +0100

    update defaults

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,8 @@ class s3fs ( $s3fs_version = '1.79',
             $fuse_version = '2.9.4',
             $tarball_url,
             $tarball_dir = '/usr/local/src',
-            $s3fs_tarball = "s3fs-${s3fs_version}.tar.gz",
-            $fuse_tarball = "fuse-${fuse_version}.tar.gz"
+            $s3fs_tarball = "s3fs-1.79.tar.gz",
+            $fuse_tarball = "fuse-2.9.4.tar.gz"
 ) {
 
   Exec {
@@ -27,7 +27,7 @@ class s3fs ( $s3fs_version = '1.79',
 
   #Install a new version of Fuse
   wget::fetch { 'fuse':
-    source      => "${tarball_url}${fuse_tarball}",
+    source      => "${tarball_url}/${fuse_tarball}",
     destination => "${tarball_dir}/${fuse_tarball}",
   }
   exec {'extract-fuse':
